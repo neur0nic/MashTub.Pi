28ea73c378c88e549277e091482059b4322c30ee
Stephan Mertens <steph_cs@gmx.de>
2016-03-20 02:07:26 +0100

    IBU Rechner funktioniert. Nur die Tabelle muss fertig geschrieben werden.

--- Code/bierrechnungen.py
+++ Code/bierrechnungen.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-def spindel(t, theta):
+def spindel(plato, theta):
+    # geeichz auf 20°C
     pass
 
 
@@ -29,36 +30,36 @@ def zucker(co2menge):
 
 
 def ibu(hopfenmenge, alphasaeure, kochdauer, ausschlagwuerze, vol):
-    kochzeiten = (5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 100, 120)
-    platos = (8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)
-    ausnutzungsgrad = (
-        (5.4, 9.9, 13.5, 16.5, 20.9, 23.8, 25.8, 27.2, 28.1, 28.7, 29.3, 29.6)
-        (5.2, 9.5, 13.0, 15.9, 20.1, 23.0, 24.9, 26.2, 27.0, 27.6, 28.3, 28.6)
-        (5.0, 9.1, 12.5, 15.3, 19.4, 22.1, 24.0, 25.2, 26.1, 26.6, 27.2, 27.5)
-        (4.8, 8.8, 12.1, 14.7, 18.7, 21.3, 23.1, 24.3, 25.1, 25.6, 26.2, 26.5)
-        (4.7, 8.5, 11.6, 14.2, 18.0, 20.5, 22.3, 23.4, 24.2, 24.7, 25.3, 25.5)
-        (4.5, 8.2, 11.2, 13.7, 17.3, 19.8, 21.4, 22.5, 23.3, 23.8, 24.3, 24.6)
-        (4.3, 7.9, 10.8, 13.1, 16.7, 19.0, 20.6, 21.7, 22.4, 22.9, 23.4, 23.7)
+    kochzeiten = [5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 100, 120]
+    platos = [
+        8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+        ]
+    ausnutzungsgrad = [
+        [5.4, 9.9, 13.5, 16.5, 20.9, 23.8, 25.8, 27.2, 28.1, 28.7, 29.3, 29.6],
+        [5.2, 9.5, 13.0, 15.9, 20.1, 23.0, 24.9, 26.2, 27.0, 27.6, 28.3, 28.6],
+        [5.0, 9.1, 12.5, 15.3, 19.4, 22.1, 24.0, 25.2, 26.1, 26.6, 27.2, 27.5],
+        [4.8, 8.8, 12.1, 14.7, 18.7, 21.3, 23.1, 24.3, 25.1, 25.6, 26.2, 26.5],
+        [4.7, 8.5, 11.6, 14.2, 18.0, 20.5, 22.3, 23.4, 24.2, 24.7, 25.3, 25.5],
+        [4.5, 8.2, 11.2, 13.7, 17.3, 19.8, 21.4, 22.5, 23.3, 23.8, 24.3, 24.6],
+        [4.3, 7.9, 10.8, 13.1, 16.7, 19.0, 20.6, 21.7, 22.4, 22.9, 23.4, 23.7]
         #nicht fertig, weiter machen bei 15°P
-        )
+        ]
     i = 0
-    while kochdauer < kochzeiten[i]:
+    while kochdauer > kochzeiten[i]:
         i += 1
     kochdauer = kochzeiten[i]
     j = 0
-    while ausschlagwuerze < platos[j]:
+    while ausschlagwuerze > platos[j]:
         j += 1
     ausschlagwuerze = platos[j]
-    for x in ausnutzungsgrad:
-        for y in ausnutzungsgrad:
-            ausnutzung = y
+    ausnutzung = ausnutzungsgrad[j][i]
     be = (hopfenmenge * alphasaeure * ausnutzung) / (10 * vol)
     be = round(be, 1)
     return be
 
-w = mischungskreuz(9, 11, 10)
-x = sudhausausbeute(11, 9, 1.55)
-y = alkoholgehalt(13, 3)
-z = ibu(10, 7.1, 60, 10, 10)
+w = 'Volumen der Anstellwürze: ' + str(mischungskreuz(9, 11, 10)) + ' l\n'
+x = 'Sudhausausbeute: ' + str(sudhausausbeute(11, 9, 1.55)) + ' %\n'
+y = 'Alkoholgehalt: ' + str(alkoholgehalt(13, 3)) + ' %Vol\n'
+z = 'Bitter: ' + str(ibu(20, 7.1, 60, 10, 10)) + ' IBU'
 
-print(w, x, y)
+print('---\n', w, x, y, z)
